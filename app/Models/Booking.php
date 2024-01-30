6a2831a88fc31f1a4530cd0558748f3c72fa564a
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2024-01-31 07:16:11 +0900

    Update Booking.php
    uppercase attribute ds

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,6 +10,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Booking extends Model
 {
     use HasFactory;
+
+    protected function ds(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => strtoupper($value));
+    }
+
 
     public function slot(): BelongsTo
     {
