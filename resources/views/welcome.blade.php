2362c5526fc508338611eecfdbc4fdba2014bd30
abiisaleh <abi.saleh.25@gmail.com>
2024-01-30 15:38:50 +0700

    update dasain

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,20 @@
 @extends("_simple")
 
 @section("content")
-        <h1>Booking Parkir<br>Online</h1>
-        <img
-          class="mb-4 mx-auto"
-          src="img/car.png"
-          alt="mobil parkir"
-          style="max-height: 300px; width: auto"
-        />
-        <div class="d-grid gap-2">
+        <h6>{{config("app.name")}}</h6>
+
+        <img 
+          src="https://media-public.canva.com/zPsmA/MACxYqzPsmA/2/tl.png" 
+          class="mx-auto" 
+          alt="mobil parkir" 
+          style="filter: invert(100%); max-width: 250px; margin: 100px 0;"
+        >
+
+        <h3>Booking Parkir</h3>
+        <p class="mb-5">booking tempat parkir langsung dari hp anda</p>
+      
+        <div class="d-grid gap-2 mt-4">
           <a href="parking" class="btn btn-light">Get Started</a>
-          <a href="admin" class="btn btn-warning">Login</a>
+          <a href="admin" class="btn btn-outline-light">Login</a>
         </div>
 @endSection
