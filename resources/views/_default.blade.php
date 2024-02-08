431f8bbb9d84c3cb811fadf3551c48b8639bda52
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2024-02-08 18:19:23 +0900

    Update _default.blade.php
    add bs icon

--- resources/views/_default.blade.php
+++ resources/views/_default.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{config("app.name")}}</title>
-    <link href="/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link href="/dist/css/bootstrap.min.css" rel="stylesheet" />
   </head>
   <body>
     <main class="container">
