ac88678209b59bf08f7b32d3e2fddf7b5c21593e
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2024-02-09 08:38:20 +0900

    Update _simple.blade.php
    add padding

--- resources/views/_simple.blade.php
+++ resources/views/_simple.blade.php
@@ -15,7 +15,7 @@
 </head>
 
 <body class="bg-dark text-white">
-  <main class="vh-100 d-flex justify-content-center align-items-center mx-auto">
+  <main class="vh-100 d-flex justify-content-center align-items-center mx-auto p-1">
     <div class="text-center">
       <h6><a href="{{url('/')}}" class="text-white text-decoration-none">{{config("app.name")}}</a></h6>
       @yield("content")
